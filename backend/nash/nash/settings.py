d08942011b2259a555f78393b38f439b41671200
byif <166938225+byif@users.noreply.github.com>
2025-12-28 19:40:49 +0530

    Add Vercel app URL to CORS allowed origins

--- backend/nash/nash/settings.py
+++ backend/nash/nash/settings.py
@@ -115,6 +115,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:8080",
     "http://127.0.0.1:8080",
+    "https://nashchat.vercel.app"
 ]
 
 SECURE_CROSS_ORIGIN_OPENER_POLICY = "same-origin-allow-popups"
